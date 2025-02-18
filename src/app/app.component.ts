83e8e025731c3ca0435de46002fdbd092f78df63
chente09 <chentenengerc@gmail.com>
2025-02-18 11:50:36 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterLink, 
+    RouterOutlet, 
+    NzIconModule, 
+    NzLayoutModule, 
+    NzMenuModule,
+    RouterModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'gestium-sli';
+  isCollapsed = false;
+
+  activeRoute = '';
+
+  menuItems = [
+    { title: 'ISSFA', route: '/issfa' },
+    { title: 'Inmobiliaria', route: '/inmobiliaria' },
+    { title: 'Banco Produbanco', route: '/produbanco' },
+    { title: 'Banco Pichincha', route: null } // Este no tiene ruta
+  ];
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.activeRoute = this.router.url; // Captura la ruta activa al iniciar
+  }
+
+  setActive(route: string | null) {
+    this.activeRoute = route ?? '';
+  }
+
+  toggleMenu() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  irWelcome() {
+    this.router.navigate(['/welcome']);
+  }
 }
